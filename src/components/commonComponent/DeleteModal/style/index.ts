c9373fe190d920493e59d77b6688a5f7c4659926
Arun-singh364 <arunsinghrajpoot364@gmail.com>
2022-11-12 19:30:43 +0530

    delete modal aded

---
+++ src/components/commonComponent/DeleteModal/style/index.ts
@@ -0,0 +1,17 @@
+import { makeStyles } from '@mui/styles';
+import { COLORS } from "../../../../constants/colors";
+
+export default makeStyles({
+  buttonContainer:{
+display:"flex",
+justifyContent:"space-around"
+  },
+  deleteButton:{
+    color:"red"
+  },
+  deleteText:{
+    marginTop:"15px",
+    marginBottom:"15px"
+
+  }
+});
