e432ece18caba9ddabb8064483b76396f9b28df7
Aditya Mohan <adityamohan@ASHWINNEW.local>
2022-12-21 22:49:33 +0530

    map icon info added

--- src/components/Map/Marker/index.js
+++ src/components/Map/Marker/index.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-import MarkerStyled from './MarkerStyled';
-import MarkerInGroupStyled from './MarkerInGroupStyled';
-import Spy from '../Spy';
-import VehicleIcons from 'assets/delivery-truck.png';
+import MarkerStyled from "./MarkerStyled";
+import MarkerInGroupStyled from "./MarkerInGroupStyled";
+import Spy from "../Spy";
+import VehicleIcons from "assets/delivery-truck.png";
+import { withStyles } from "@material-ui/core/styles";
+import { Box, Button, Typography, Zoom } from "@mui/material";
+import Tooltip from "@mui/material/Tooltip";
+
+const HtmlTooltip = withStyles((theme) => ({
+  tooltip: {
+    color: "rgba(0, 0, 0, 0.87)",
+    maxWidth: 220,
+    fontSize: theme.typography.pxToRem(12),
+    border: "1px solid #ed6929",
+  },
+}))(Tooltip);
 
 class Marker extends React.PureComponent {
   // eslint-disable-line react/prefer-stateless-function
@@ -14,14 +26,36 @@ class Marker extends React.PureComponent {
 
   render() {
     return (
-      <div > 
-        {this.props.inGroup
-          ? <MarkerInGroupStyled>
-             <img src={VehicleIcons} width={60} alt="" />
-            </MarkerInGroupStyled>
-          : <MarkerStyled>
+      <div>
+        {this.props.inGroup ? (
+          <MarkerInGroupStyled>
+            <HtmlTooltip
+              title={
+                <React.Fragment>
+                  <Typography color="inherit">Driver Info</Typography>
+                  <em>{"100KM"}</em> <b>{"some"}</b> <u>{"amazing content"}</u>.{" "}
+                  {"It's very engaging. Right?"}
+                </React.Fragment>
+              }
+            >
               <img src={VehicleIcons} width={60} alt="" />
-            </MarkerStyled>}
+            </HtmlTooltip>
+          </MarkerInGroupStyled>
+        ) : (
+          <MarkerStyled>
+            <HtmlTooltip
+              title={
+                <React.Fragment>
+                  <Typography color="inherit">Driver Info</Typography>
+                  <em>{"100KM"}</em> <b>{"some"}</b> <u>{"amazing content"}</u>.{" "}
+                  {"It's very engaging. Right?"}
+                </React.Fragment>
+              }
+            >
+              <img src={VehicleIcons} width={60} alt="" />
+            </HtmlTooltip>
+          </MarkerStyled>
+        )}
       </div>
     );
   }
