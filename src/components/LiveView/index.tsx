035823d47e379f30aa443a85c5c25b4b3f8816b6
chetan chauhan <chetanrajputsm@gmail.com>
2022-12-28 23:05:13 +0530

    pagination in map and live view vehicle list

--- src/components/LiveView/index.tsx
+++ src/components/LiveView/index.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
-import { Snackbar, Alert } from "@mui/material";
+import { Snackbar, Alert, TableRow } from "@mui/material";
 import useStyles from "./style";
 import Heading from "components/commonComponent/Heading";
 import Paper from "@mui/material/Paper";
@@ -11,10 +11,15 @@ import mapIcon from "../../assets/location.png";
 import { monitor, transport } from "constants/RouteMiddlePath";
 import { useQuery } from "react-query";
 import client from "serverCommunication/client";
-import { Button, List, ListItemText } from "@mui/material";
+import { Button, List, ListItemText, SelectChangeEvent } from "@mui/material";
 import SerachIcon from "../../assets/search-icon.png";
 import notFound from "../../assets/404.jpg";
 import Iframe from "react-iframe";
+import Table from "@mui/material/Table";
+import {
+  TableFooter,
+} from "components/commonComponent/Table";
+
 
 export default function () {
   const Item = styled(Paper)(({ theme }) => ({
@@ -34,7 +39,7 @@ export default function () {
 
   const [searchText, setSearchText] = useState("");
   const [page, setPage] = useState(0);
-  const [rowsPerPage, setRowsPerPage] = useState(1000);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
   const [deviceId, setDeviceId] = useState("");
   const [videoUrl, setVideoUrl] = useState("");
   const [selectedDevice, setSelectedDevice] = useState<string[]>([]);
@@ -43,15 +48,22 @@ export default function () {
     ["vehiclelist", page, rowsPerPage, searchText, deviceId],
     () => getVehicles(page, rowsPerPage, searchText)
   );
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage - 1);
+  };
+
+  const handleChangeRowsPerPage = (event: SelectChangeEvent) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
 
   async function getVehicles(
     pageNumber: number,
     pageSize: number,
     searchText?: string
   ) {
-    let getApiUrl = `${transport}/vehicles/?page=${
-      pageNumber + 1
-    }&page_size=${pageSize}&search=${searchText}`;
+    let getApiUrl = `${transport}/vehicles/?page=${pageNumber + 1
+      }&page_size=${pageSize}&search=${searchText}`;
 
     const response = await client.get(getApiUrl);
 
@@ -136,41 +148,54 @@ export default function () {
                   </List>
                   <Box className="notfound">
                     <div className="contendata">
-                      {!isVehicleLoading &&
-                        vehicleList?.results.map((item: any, index: number) => (
-                          <div
-                            className="loaddata"
-                            style={
-                              selectedDevice.includes(item.device)
-                                ? { background: "#fef8f0" }
-                                : {}
-                            }
-                          >
-                            <i className="circle"></i>
-                            <span className="trackid">{item.vin}</span>
-                            <span
-                              className="arrowright"
-                              onClick={() => {
-                                handleVehicleView(item.device);
-                              }}
-                            >
-                              <svg
-                                width="17"
-                                height="15"
-                                fill="none"
-                                xmlns="http://www.w3.org/2000/svg"
+                      {!isVehicleLoading && <div>
+                        <Table>
+                          {vehicleList?.results.map((item: any, index: number) => (
+                            <TableRow>
+                              <div
+                                className="loaddata"
+                                style={
+                                  selectedDevice.includes(item.device)
+                                    ? { background: "#fef8f0" }
+                                    : {}
+                                }
                               >
-                                <path
-                                  d="M15.75 7.726h-15M9.7 1.701l6.05 6.024L9.7 13.75"
-                                  stroke="#3BB3C3"
-                                  stroke-width="1.5"
-                                  stroke-linecap="round"
-                                  stroke-linejoin="round"
-                                ></path>
-                              </svg>
-                            </span>
-                          </div>
-                        ))}
+                                <i className="circle"></i>
+                                <span className="trackid">{item.vin}</span>
+                                <span
+                                  className="arrowright"
+                                  onClick={() => {
+                                    handleVehicleView(item.device);
+                                  }}
+                                >
+                                  <svg
+                                    width="17"
+                                    height="15"
+                                    fill="none"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                  >
+                                    <path
+                                      d="M15.75 7.726h-15M9.7 1.701l6.05 6.024L9.7 13.75"
+                                      stroke="#3BB3C3"
+                                      stroke-width="1.5"
+                                      stroke-linecap="round"
+                                      stroke-linejoin="round"
+                                    ></path>
+                                  </svg>
+                                </span>
+                              </div>
+                            </TableRow>
+                          ))}
+                        </Table>
+                        <TableFooter
+                          totalPages={Math.ceil(vehicleList?.count / rowsPerPage)}
+                          currentPage={page + 1}
+                          onPageChange={handleChangePage}
+                          rowsPerPage={rowsPerPage}
+                          onChangeRowsPerPage={handleChangeRowsPerPage}
+                        />
+                      </div>}
+
                       {!isVehicleLoading && !vehicleList?.results.length && (
                         <div className="notfoundimg">
                           {" "}
@@ -194,9 +219,8 @@ export default function () {
                   }}
                 >
                   <Iframe
-                    url={`https://livefeed.shoora.com/videofeed/${
-                      videoUrl == "" ? "?device=" : videoUrl
-                    }&email=its@its.com&password=123456`}
+                    url={`https://livefeed.shoora.com/videofeed/${videoUrl == "" ? "?device=" : videoUrl
+                      }&email=its@its.com&password=123456`}
                     position="relative"
                     width="100%"
                     id="myId"
